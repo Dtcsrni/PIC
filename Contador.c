d7f0bc8a927bd50e8def26b144d9e3179b497ebe
Erick Vega <evega-12@hotmail.com>
2019-03-29 02:40:58 -0600

    Update Contador.c

--- Contador.c
+++ Contador.c
@@ -24,36 +24,37 @@ sbit LCD_D7_Direction at TRISB4_bit;
 //Variables de apoyo
    int activado1=0;         //Bandera de activado
    int activado2=0;
-   int desactivado1=0;         //Bandera de activado
+   int desactivado1=0;         //Bandera de desactivado
    int desactivado2=0;
-   int contador1=0;
+   int contador1=0;      //variables de contador
    int contador2=0;
    int Lactiv=0;
+   //Variables de texto
    char txt[8];
-   char txt1[8]="P1";
-   char txt3[8]="P2";
+   char txt1[8]="P1";   //Texto P1
+   char txt3[8]="P2";   //Texto P2
    char txt2[8];
 void main() {
 
-       ANSEL = 0b00000000; // Digital
+       ANSEL = 0b00000000; // Pines analógicos como digitales
        ANSELH = 0;
        TRISE = 0b00000011; // PORTE.0 y PORTE.1 son configurados como entradas. Los demás como salidas
-       PORTE = 0;
-       TRISD = 0b00000000; // PORTE.0 y PORTE.1 son configurados como entradas. Los demás como salidas
-       PORTD = 0;
-        Lcd_Init();
-        Delay_ms(500);
-        Lcd_Out(1,1,"   ||CUH||");
+       PORTE = 0;                     //Se limpia el puerto
+       TRISD = 0b00000000; // PORTD como salidas
+       PORTD = 0;                     //Se limpia el puerto
+        Lcd_Init();                  //Se inicializa el módulo lcd
+        Delay_ms(500);                  //Espera de estabilización de LCD
+        Lcd_Out(1,1,"   ||CUH||");         //Impresión de Bienvenida
         Lcd_Out(2,1,"||ERICK VEGA||");
         Delay_ms(2000);
                // Cursor off
-         Lcd_Cmd(_LCD_CLEAR);
-         Lcd_Cmd(_LCD_CURSOR_OFF);
+         Lcd_Cmd(_LCD_CLEAR);        //Limpiar pantalla
+         Lcd_Cmd(_LCD_CURSOR_OFF);     //Ocultar cursor
 
-             PORTD.RD2=1;
+             PORTD.RD2=1;     //Prender LED en RD2
       while(1){
       Lcd_Cmd(_LCD_CLEAR);
-      if(Lactiv==0){
+      if(Lactiv==0){        //Prender y apagar led en cada iteración, turnandose
       PORTD.RD2=1;
       Lactiv=1;
       }
@@ -63,7 +64,7 @@ void main() {
         Lactiv=0;
       }
         }
-            if(PORTE.RE0 ==0){
+            if(PORTE.RE0 ==0){    //Si se detecta interrupción de entrada en RE0
                activado1=1;
             }
             else{
@@ -71,7 +72,7 @@ void main() {
                activado1=0;
             }
 
-            if(PORTE.RE1 ==0){
+            if(PORTE.RE1 ==0){   //Si se detecta interrupción de entrada en RE1
                activado2=1;
             }
             else{
@@ -79,7 +80,7 @@ void main() {
                activado2=0;
             }
 
-      if(activado1==1&&desactivado1==0){
+      if(activado1==1&&desactivado1==0){     //Si está activado y ya se ha removido la obstrucción
                  contador1=contador1+1;
                  desactivado1=1;
                }
@@ -90,7 +91,7 @@ void main() {
                }      }
                WordToStr(contador1, txt);
                WordToStr(contador2, txt2);
-      Lcd_Out(1,1,txt1);
+      Lcd_Out(1,1,txt1);        //Mostrar texto y cuentas en pantalla
       Delay_us(1000);
       Lcd_Out(1,8,txt);
       Delay_us(1000);
